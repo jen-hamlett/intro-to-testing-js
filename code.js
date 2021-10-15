a0878f0cd1778c8ecbd05d4e71ac08eaa80241be
Jen HAmlett <jenhamlettwilkerson@gmail.com>
2021-10-15 16:19:57 -0500

    unittest

--- code.js
+++ code.js
@@ -11,3 +11,27 @@ function itsFive(input) {
     return (input == 5);
 
 }
+function isEven(input) {
+    if (parseFloat(input) && input != Infinity) return !(input % 2);
+    else return false;
+}
+
+function isVowel(input) {
+
+    if (input === "" || (typeof input != "string")) {
+        return false;
+    }
+    input = input.toLowerCase();
+
+    return ((input.length == 1) && (input === "a" || input === ("e")
+        || input === ("i") || input ===("O") || input ===("u") || input === ("y") ));
+
+}
+function add(num1, num2) {
+
+    if ((typeof num1 && typeof num2) != 'number') return NaN;
+    else return (num1 + num2);
+
+
+}
+console.log(add() + " nothing")
--- tests.js
+++ tests.js
@@ -35,19 +35,111 @@ describe( 'sayHello', function() {
 
 //unit test for itsFive
 describe( 'itsFive', function() {
-    it('should be a defined function', function() {
-        expect( typeof itsFive).toBe('function');
+    it('should be a defined function', function () {
+        expect(typeof itsFive).toBe('function');
     });
     it('should return a boolean', function () {
-        expect( typeof itsFive()).toBe( 'boolean');
+        expect(typeof itsFive()).toBe('boolean');
     });
-    it('should return true when passed input of 5', function()  {
-        expect( itsFive(5)).toBe(true);
+    it('should return true when passed input of 5', function () {
+        expect(itsFive(5)).toBe(true);
     });
-    it('should return true when passed input of string 5', function()  {
-        expect (itsFive("5")).toBe(true);
+    it('should return true when passed input of string 5', function () {
+        expect(itsFive("5")).toBe(true);
+    });
+});
+
+    //unit Test exercise 11  itsEven
+describe( "itsEven", function() {
+    it("should be a defined function", function()  {
+        expect( typeof isEven).toBe( 'function');
+        });
+    it("should return a boolean", function()  {
+        expect( typeof isEven()).toBe( 'boolean');
+    });
+    it("should return true when passed input of 2", function()  {
+        expect( isEven(2)).toBe( true);
+    });
+    it("should return false  when passed input of -4", function()  {
+        expect( isEven(-4)).toBe( true);
+    });
+    it("should return false  when passed input of 3", function()  {
+        expect( isEven(3)).toBe( false);
+    });
+    it("should return false  when passed input of string banana", function()  {
+        expect( isEven('banana')).toBe( false);
+    });
+    it("should return false  when passed input of 8", function()  {
+        expect( isEven(8)).toBe( true);
+    });
+    it("should return false  when passed input of Infinity", function()  {
+        expect( isEven(Infinity)).toBe( false);
+    });
+    it("should return false  when passed no argument", function()  {
+        expect( isEven("")).toBe( false);
     });
 
+});
+
+//Unit test exercise number 12 isVowel
+describe( "isVowel", function() {
+    it("should be a defined function", function()  {
+        expect( typeof isVowel).toBe( 'function');
+    });
+    it("should return a boolean", function()  {
+        expect( typeof isVowel()).toBe( 'boolean');
+    });
+    it("should return true when passed input of a", function()  {
+        expect( isVowel("a")).toBe( true);
+    });
+    it("should return true  when passed input of A", function()  {
+        expect( isVowel("A")).toBe( true);
+    });
+        it("should return false  when passed input of 4", function()  {
+            expect( isVowel(4)).toBe( false);
+        });
+    it("should return false  when passed input of B", function()  {
+        expect( isVowel("B")).toBe( false);
+    });
+    it("should return true  when passed input of y", function()  {
+        expect( isVowel("y")).toBe( true);
+    });
+    it("should return true  when passed input of e", function()  {
+        expect( isVowel("e")).toBe( true);
+    });
+    it("should return false  when passed input of a boolean value", function()  {
+            expect( isVowel(true)).toBe( false);
+            expect( isVowel(false)).toBe(false);
+    });
+    it("should return false  when passed input of string banana", function()  {
+        expect( isVowel('banana')).toBe( false);
+    });
+    it("should return false when passed no input", function() {
+        expect( isVowel()).toBe(false);
+    });
+});
+
+    //Unit test exercise number 13 add
+
+describe( "add", function() {
+    it("should be a defined function", function()  {
+        expect( typeof add).toBe( 'function');
+    });
+    it("should return a boolean", function()  {
+        expect( typeof add()).toBe( 'number');
+    });
+    it("should return true when passed input of 2, 3", function()  {
+        expect( add(2, 3)).toBe( 5);
+    });
+    it("should return true when passed input of -4, 10", function()  {
+        expect( add(-4, 10)).toBe( 6);
+    });
+    it("should return true when passed input of -3 9", function()  {
+        expect( add(-3, -9)).toBe( -12);
+    });
+    it("should return true when passed input of 2, apples", function()  {
+        expect( add(2, 'apples')).toBeNaN();
+    });
 
 
 });
