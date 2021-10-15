4c90521db5e17ab5901a57c4b228ac95bdbd9b7e
Jen HAmlett <jenhamlettwilkerson@gmail.com>
2021-10-15 13:53:56 -0500

    unit testing

--- code.js
+++ code.js
@@ -2,3 +2,12 @@
 function helloWorld() {
     return "Hello, World!";
 }
+// sayHello function
+function sayHello(name) {
+    return "Hello " + name;
+}
+function itsFive(input) {
+    input = parseInt(input);
+    return (input == 5);
+
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,41 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+
+//unit test for sayHello
+describe( 'sayHello', function() {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should never return undefined', function () {
+        expect(typeof sayHello()).not.toBe('undefined');
+    });
+    it('should return a string', function () {
+        expect(typeof sayHello()).toBe('string');
+    });
+    it('should return correct greeting', function () {
+        expect( sayHello('Jen')).toBe('Hello Jen');
+        expect( sayHello('Bob')).toBe('Hello Bob');
+    });
+
+});
+
+//unit test for itsFive
+describe( 'itsFive', function() {
+    it('should be a defined function', function() {
+        expect( typeof itsFive).toBe('function');
+    });
+    it('should return a boolean', function () {
+        expect( typeof itsFive()).toBe( 'boolean');
+    });
+    it('should return true when passed input of 5', function()  {
+        expect( itsFive(5)).toBe(true);
+    });
+    it('should return true when passed input of string 5', function()  {
+        expect (itsFive("5")).toBe(true);
+    });
+
+
+
+});
